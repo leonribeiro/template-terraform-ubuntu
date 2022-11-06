240b6480e77923f44ed7f0d456d8a897065ac8d2
Leon <leonribeiro10@gmail.com>
2022-11-06 11:15:26 -0300

    Ubuntu Server #1

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c09c7eb16d3e8e70"
+  instance_type = "t2.micro"
+  key_name = "iac"
+
+  tags = {
+    Name = "ubuntu-server"
+  }
+}
